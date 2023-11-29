515f49049682f6b47909d68bb9d2aa842ac01015
NezBut <xyi12312354@mail.ru>
2023-11-30 00:45:59 +0700

    requests to add to the database

---
+++ database/add_requests.py
@@ -0,0 +1,56 @@
+import aiosqlite
+import config
+import logging
+
+
+class AddDataBase:
+
+    @staticmethod
+    async def add_stat_user(user: str, count_games: int = 0, wins: int = 0):
+        """
+        Add the game statistics for a user to the database.
+
+        Args:
+            user (str): The username of the user.
+            count_games (int, optional): The number of games played. Defaults to 0.
+            wins (int, optional): The number of wins. Defaults to 0.
+        """
+        try:
+            async with aiosqlite.connect(config.DATA_BASE_DIR) as db:
+                db.row_factory = aiosqlite.Row
+                await db.execute('INSERT INTO users_stat (user, count_games, wins) VALUES (?, ?, ?)',
+                                 (user.lower().strip(), str(count_games), str(wins)))
+                await db.commit()
+
+        except Exception as e:
+            logging.error(e)
+            return False
+
+        else:
+            return True
+
+    @staticmethod
+    async def add_user(user: str, chat_id: str):
+        """
+        Add the user to the database.
+
+        Args:
+            user (str): The username of the user.
+            chat_id (str): The chat id of the user.
+        """
+
+        try:
+            async with aiosqlite.connect(config.DATA_BASE_DIR) as db:
+                db.row_factory = aiosqlite.Row
+
+                await db.execute('INSERT INTO users (username, chat_id) VALUES (?, ?)',
+                                 (user.lower().strip(), chat_id))
+
+                await db.commit()
+
+        except Exception:
+            logging.error(e)
+            return False
+
+        else:
+            return True
