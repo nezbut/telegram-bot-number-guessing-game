16bbae7691634fb53b50ce7bc97b68c8adff9924
NezBut <xyi12312354@mail.ru>
2023-12-02 20:19:41 +0700

    main bot launch file

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from handlears import dp, bot
+from aiogram import Bot, Dispatcher
+from handlears import commands_handlears, other_handlears
+from config.config_data import Config
 import asyncio
 import logging
 
@@ -7,9 +9,18 @@ async def main():
     """
     Main entry point of the program.
     """
-    logging.basicConfig(level=logging.INFO)
+    bot = Bot(token=Config.telegram_bot.bot_token)
+    dp = Dispatcher()
+
+    dp.include_routers(
+        commands_handlears.router,
+        other_handlears.router
+    )
+
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
