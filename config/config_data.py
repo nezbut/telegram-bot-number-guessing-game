a2785757a1462ed1410d7f89add99bd3899691c7
NezBut <xyi12312354@mail.ru>
2023-12-04 17:35:13 +0700

    config data file

--- config/config_data.py
+++ config/config_data.py
@@ -17,20 +17,32 @@ class BotDataBase:
 class ConfigData:
     telegram_bot: TelegramBot
     bot_db: BotDataBase
+    command_menu: bool
 
+def _get_choose_command_menu() -> bool:
+    menu = getenv('COMMAND_MENU').upper()
+    menu_choose = menu if menu in ('YES', 'NO') else 'NO'
 
-def load_config() -> ConfigData:
+    match menu_choose:
+
+        case "YES":
+            return True
+
+        case "NO":
+            return False
+
+def _load_config() -> ConfigData:
     return ConfigData(
         telegram_bot=TelegramBot(
             bot_token=getenv('BOT_TOKEN')
         ),
-
         bot_db=BotDataBase(
             data_base_path=Path(__file__).resolve().parent.parent / 'database' / 'db.db'
-        )
+        ),
+        command_menu=_get_choose_command_menu()
     )
 
-Config = load_config()
+Config = _load_config()
 
 __all__ = (
     'Config',
